8a9e765aed8297379b6535ea5a325b2b639214a2
Usuario <usuario@MacBook-Air-de-Usuario.local>
2023-07-01 12:52:12 +0200

    fix: PDF fix

--- src/components/PdfViewer/index.jsx
+++ src/components/PdfViewer/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
-import 'react-pdf/dist/esm/entry.webpack';
+import { ArrowLeftShort, ArrowRightShort } from 'react-bootstrap-icons';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
@@ -21,17 +21,22 @@ export default function PdfViewer({file, isPreview}) {
   function onDocumentLoadSuccess({ numPages: nextNumPages }) {
     setNumPages(nextNumPages);
   }
-
+  const onClickBack = () => currentPage === 1 ? null : setCurrentPage(currentPage - 1)
+  const onClickForward = () => currentPage === numPages ? null : setCurrentPage(currentPage + 1)
   return (
     <div className='pdf-container'>
           <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
             {isPreview ?
               (
               <>
-              <Page renderTextLayer={false} pageNumber={currentPage} />
-              <div className='pdf-controls-container'>
-
-              </div>
+              <Page renderTextLayer={false} pageNumber={currentPage}>
+                <div className='pdf-controls-container'>
+                  <ArrowLeftShort size={20} className={currentPage === 1 ? 'arrow-bar arrow-bar-disabled' : 'arrow-bar'} onClick={onClickBack}/>
+                  <span className='page-text'>{currentPage} | {numPages}</span>
+                  <ArrowRightShort size={20} className={currentPage === numPages ? 'arrow-bar arrow-bar-disabled' : 'arrow-bar'} onClick={onClickForward}/>
+                </div>
+              </Page>
+              
               </>) :  <Page renderTextLayer={false}  pageNumber={1} />}
           </Document>
     </div>
